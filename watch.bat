c8a973e0ae11f8e9dd86d62df9558b4c0c6e21c6
Andrew Lynch <a_t_lynch@protonmail.ch>
2022-08-06 01:07:14 -0500

    Add build scripts

---
+++ watch.bat
@@ -0,0 +1,4 @@
+set plugin_name=Test
+set plugins_path=%LOCALAPPDATA%\Roblox\Plugins
+
+rojo build --watch -o %plugins_path%\%plugin_name%.rbxm
---
+++ watch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+plugin_name="Test"
+plugins_path="~/Documents/ROBLOX/Plugins"
+
+rojo build --watch -o "$plugins_path/$plugin_name.rbxm"
